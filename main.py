66ef883b95b79f0dc904ecbfc7ab277cc362694e
Herick Souza <86500252+HerickSz@users.noreply.github.com>
2023-01-26 21:55:35 -0300

    update v2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import discord
 from discord import app_commands
 
-id_do_servidor = 858763426678571039
+id_do_servidor = #idserver
 
 
 class client(discord.Client):
 
   def __init__(self):
     super().__init__(intents=discord.Intents.default())
-    self.synced = False  #Nós usamos isso para o bot não sincronizar os comandos mais de uma vez
+    self.synced = False  
 
   async def on_ready(self):
     await self.wait_until_ready()
-    if not self.synced:  #Checar se os comandos slash foram sincronizados
+    if not self.synced:  
       await tree.sync(
         guild=discord.Object(id=id_do_servidor)
-      )  # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
+      )  
       self.synced = True
     print(f"Entramos como {self.user}.")
 
@@ -26,10 +26,10 @@ tree = app_commands.CommandTree(aclient)
 
 @tree.command(guild=discord.Object(id=id_do_servidor),
               name='oi',
-              description='...')  #Comando específico para seu servidor
+              description='...') 
 async def slash2(interaction: discord.Interaction):
   await interaction.response.send_message(f"Olá!!", ephemeral=True)
 
 
 aclient.run(
-  'MTA1OTI0OTY3MTg4MjY4MjQ4OQ.GEJ8vJ.m3JNMd8aU1H13DxKOsQjnqtnjijvxK2gtHIVbQ')
+  'code bot')
